5765282217dfe57a53abfe49391aaa30528a1675
hirwaishimwe <118475346+hirwaishimwe@users.noreply.github.com>
2023-04-02 21:27:31 -0400

    Update insertion_sort_linked_lists.py

--- insertion_sort_linked_lists.py
+++ insertion_sort_linked_lists.py
@@ -6,6 +6,8 @@
 #This code usese the inerstion sort algorithm to sort the  
 #elements of the list in ascending order 
 ######################################
+import time
+
 class Node:
     def __init__(self, data=None):
         self.data = data
@@ -14,16 +16,16 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         
     def insert(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
         else:
-            curr_node = self.head
-            while curr_node.next is not None:
-                curr_node = curr_node.next
-            curr_node.next = new_node
+            self.tail.next = new_node
+            self.tail = new_node
     
     def print_list(self):
         curr_node = self.head
@@ -36,27 +38,20 @@ class LinkedList:
         if self.head is None:
             return
         
-        sorted_list = LinkedList()
-        curr_node = self.head
+        curr_node = self.head.next
+        self.head.next = None
         while curr_node is not None:
             next_node = curr_node.next
-            sorted_list.insert_sorted(curr_node.data)
+            if curr_node.data < self.head.data:
+                curr_node.next = self.head
+                self.head = curr_node
+            else:
+                prev_node = self.head
+                while prev_node.next is not None and prev_node.next.data < curr_node.data:
+                    prev_node = prev_node.next
+                curr_node.next = prev_node.next
+                prev_node.next = curr_node
             curr_node = next_node
-        
-        self.head = sorted_list.head
-    
-    def insert_sorted(self, data):
-        new_node = Node(data)
-        if self.head is None or self.head.data >= data:
-            new_node.next = self.head
-            self.head = new_node
-        else:
-            curr_node = self.head
-            while curr_node.next is not None and curr_node.next.data < data:
-                curr_node = curr_node.next
-            new_node.next = curr_node.next
-            curr_node.next = new_node
-
 
 
 if __name__ == '__main__':
@@ -74,9 +69,14 @@ if __name__ == '__main__':
     print("Original list:")
     linked_list.print_list()
 
-    # Sort the linked list using insertion sort
+    # Time the sorting process
+    start_time = time.time()
     linked_list.insertion_sort()
+    end_time = time.time()
 
     # Print the sorted list
     print("Sorted list:")
     linked_list.print_list()
+
+    # Print the time taken for sorting
+    print(f"Time taken: {end_time - start_time} seconds")
